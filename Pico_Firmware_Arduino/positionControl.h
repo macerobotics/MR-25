ee35a3eed06be56e134562e24cd283c3fc32acec
Mace Robotics <contact@macerobotics.com>
2026-02-11 09:37:09 +0100

    Create positionControl.h

---
+++ Pico_Firmware_Arduino/positionControl.h
@@ -0,0 +1,21 @@
+// copyleft 
+
+#ifndef POSITION_CONTROL_h
+#define POSITION_CONTROL_h
+
+// Nombre de ticks pour 1 tour de roue
+#define ENCODER_RESOLUTION 4200
+
+
+void positionControl_manage();
+
+int positionRobotX(void);
+int positionRobotY(void);
+
+// vitesse des roues
+int speedwheelRight(void);
+int speedwheelLeft(void);
+
+
+#endif
+// End of file
